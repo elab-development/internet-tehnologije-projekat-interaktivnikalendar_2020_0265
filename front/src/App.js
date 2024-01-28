408d2680ebd23b5260c758f2b04aae46ebbd0dc0
ksenija0137 <ksenijapetricevic42@gmail.com>
2024-01-28 21:08:40 +0100

    Ubacen navbar

--- front/src/App.js
+++ front/src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import Calendar from './components/Calendar';
+import Navigacija from './components/Navigacija';
 
 function App() {
   return (
     <div className="App">
+      <Navigacija />
      <Calendar />
     </div>
   );
