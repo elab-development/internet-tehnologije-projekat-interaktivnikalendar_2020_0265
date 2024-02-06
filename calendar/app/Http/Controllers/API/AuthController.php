94bd864fb106ed9ccda785409d47570961c64f32
petargavovic <stoniminetar@gmail.com>
2024-02-06 12:57:58 +0100

    Laravel <-> React: Login

--- calendar/app/Http/Controllers/API/AuthController.php
+++ calendar/app/Http/Controllers/API/AuthController.php
@@ -35,13 +35,13 @@ class AuthController extends Controller
     public function login(Request $request)
     {
         if(!Auth::attempt($request->only('email','password')))
-        return response()->json(['message' => 'Unauthorized'], 401);
+        return response()->json(['success'=> false, 'message' => 'Unauthorized'], 401);
 
         $user = User::where('email', $request['email'])->firstOrFail();
 
         $token = $user->createToken('auth_token')->plainTextToken;
 
-        return response()->json(['message'=>'Hi, '.$user->name. 'welcome to home.', 'access_token'=>$token, 'token_type'=>'Bearer']);
+        return response()->json(['success'=> true, 'message'=>'Hi, '.$user->name. 'welcome to home.', 'access_token'=>$token, 'token_type'=>'Bearer']);
     }
 
     public function logout()
--- front/src/components/Login.jsx
+++ front/src/components/Login.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Link , useNavigate} from 'react-router-dom';
 import Button from './Button';
 import { useState } from 'react';
+import axios from "axios";
 
 const Login = () => {
     
@@ -11,11 +12,20 @@ const Login = () => {
     const [password, setPassword] = useState('');
     const [showPopup, setShowPopup] = useState(false);
   
-    const handleLogin = () => {
+    const handleLogin = (e) => {
+      e.preventDefault();
+      axios.post("http://127.0.0.1:8000/api/login", {email: username,
+    password: password}).then((res)=>{console.log(res.data);
+    if(res.data.success === true){
+      window.sessionStorage.setItem("auth_token", res.data.access_token);
+      navigate('/calendar');
+    }
+    }).catch((e)=>{
+      console.log(e);
+    });
       if (username.trim() === '' || password.trim() === '') {
         alert('Please enter both username and password.');
       } else {
-        navigate('/calendar');
       }
     };
     
